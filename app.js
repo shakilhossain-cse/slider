aca9abc774fcda5e10b62ede7c73748226aae38e
Shakil Hossain <shakilhossainp.cse@gmail.com>
2021-09-11 12:04:54 +0600

    display image on ui

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-const searchInput = document.getElementById('searchInput');
-const card = document.getElementById('card');
+const searchInput = document.getElementById("searchInput");
+const searchBtn = document.getElementById('searchBtn')
+const card = document.getElementById("card");
 
-searchInput.addEventListener('keyup',(e)=>{
-    const input = e.target.value;
-    const API_URL = `https://pixabay.com/api/?key=23335201-2d2cf8ddd979f5a8bd62aa3ba&q=${input}_type=photo`
-   fetch(API_URL)
-   .then(res => res.json())
-   .then(data => showData(data))
-})
+searchBtn.addEventListener("click", () => {
+    card.textContent = '';
+  const query = searchInput.value;
+  const API_URL = `https://pixabay.com/api/?key=23335201-2d2cf8ddd979f5a8bd62aa3ba&q=${query}&image_type=photo&pretty=true`
 
-const showData = data =>{
+  fetch(API_URL)
+    .then((res) => res.json())
+    .then((data) => showData(data));
+});
 
-}
+const showData = (data) => {
+    if (data.hits.length  > 1) {       
+        data.hits.forEach(imageData => {
+
+            const col = document.createElement("div");
+            col.classList.add("col");
+            col.innerHTML = `
+              <div class="card">
+                <img src="${imageData.largeImageURL}" class="card-img-top" alt="...">
+              </div>
+            `;
+            card.appendChild(col)
+        });
+   
+    }
+};
